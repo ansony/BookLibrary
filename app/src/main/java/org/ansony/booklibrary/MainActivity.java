6f050e09bb12a2dcc20e68d376794876f30e92b2
ansony <ansony@ansony-2.local>
2014-10-04 20:24:08 +0900

    fix tabs spaces

---
+++ app/src/main/java/org/ansony/booklibrary/MainActivity.java
@@ -0,0 +1,56 @@
+package org.ansony.booklibrary;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+
+public class MainActivity extends FragmentActivity{
+
+    private static final int REGIST_BUTTON =1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_register:
+                IntentIntegrator.initiateScan(MainActivity.this);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (scanResult != null) {
+            Log.d("qrtest", "code = " + scanResult.getContents());
+            Log.d("qrtest", "format = " + scanResult.getFormatName());
+            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+            intent.putExtra("code", scanResult.getContents());
+            startActivity(intent);
+        }
+    }
+}
---
+++ app/src/main/java/org/ansony/booklibrary/RegisterActivity.java
@@ -0,0 +1,16 @@
+package org.ansony.booklibrary;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by ansony on 14/10/04.
+ */
+public class RegisterActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+}
